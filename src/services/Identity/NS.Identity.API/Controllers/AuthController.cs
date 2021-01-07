e4aa7fecf7b65f1d1f878c8a9a8830c72f3d44c2
Alexandre Mutti <alexmutti@gmail.com>
2021-01-06 22:40:03 -0300

    refactor auth flow and startup

--- src/services/Identity/NS.Identity.API/Controllers/AuthController.cs
+++ src/services/Identity/NS.Identity.API/Controllers/AuthController.cs
@@ -1,39 +1,31 @@
-﻿using System;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
-using Microsoft.IdentityModel.Tokens;
-using NS.Identity.API.Extensions;
 using NS.Identity.API.Models;
+using NS.Identity.API.Services;
 
 namespace NS.Identity.API.Controllers
 {
-    [ApiController]
     [Route("api/identiy")]
-    public class AuthController : Controller
+    public class AuthController : MainController
     {
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
-        private readonly AppSettings _appSettings;
+        private readonly IJwtService _jwtService;
 
-        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager, 
-            IOptions<AppSettings> appSettings)
+        public AuthController(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager,
+            IJwtService jwtService)
         {
             _signInManager = signInManager;
             _userManager = userManager;
-            _appSettings = appSettings.Value;
+            _jwtService = jwtService;
         }
 
         [HttpPost("register-account")]
         public async Task<ActionResult> Register(UserRegisterViewModel userRegisterViewModel)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+                return CustomResponse(ModelState);
 
             var newUser = new IdentityUser()
             {
@@ -44,86 +36,40 @@ namespace NS.Identity.API.Controllers
 
             var result = await _userManager.CreateAsync(newUser, userRegisterViewModel.Password);
 
-            if (!result.Succeeded)
-                return BadRequest();
+            foreach (var error in result.Errors)
+            {
+                AddError(error.Description);
+            }
 
-            await _signInManager.SignInAsync(newUser, false);
+            return CustomResponse();
 
-            return Ok();
         }
 
         [HttpPost("auth")]
         public async Task<ActionResult> Login(UserLoginViewModel userLoginViewModel)
         {
             if (!ModelState.IsValid)
-                return BadRequest();
+                return CustomResponse();
 
             var result =
                 await _signInManager.PasswordSignInAsync(userLoginViewModel.Email, userLoginViewModel.Password, false,
                     true);
 
-            if (!result.Succeeded)
-                return BadRequest();
-
-            return Ok(await GenerateJwt(userLoginViewModel.Email));
-        }
-
-        private async Task<UserResponseLoginModel> GenerateJwt(string email)
-        {
-            var user = await _userManager.FindByEmailAsync(email);
-            var claims = await _userManager.GetClaimsAsync(user);
-            var userRoles = await _userManager.GetRolesAsync(user);
-
-            claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.Id));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Email, user.Email));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Nbf, ToUnixEpochDate(DateTime.UtcNow).ToString()));
-            claims.Add(new Claim(JwtRegisteredClaimNames.Iat, ToUnixEpochDate(DateTime.UtcNow).ToString(), 
-                ClaimValueTypes.Integer64));
-
-            foreach (var userRole in userRoles)
+            if (result.Succeeded)
             {
-                claims.Add(new Claim("role", userRole));
+                var jwt = await _jwtService.GenerateJwt(userLoginViewModel.Email);
+                return CustomResponse(jwt);
             }
 
-            var identityClaims = new ClaimsIdentity();
-            identityClaims.AddClaims(claims);
-
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
-
-            var token = tokenHandler.CreateToken(new SecurityTokenDescriptor
+            if (result.IsLockedOut)
             {
-                Issuer = _appSettings.Issuer,
-                Audience = _appSettings.ValidAt,
-                Subject = identityClaims,
-                Expires = DateTime.UtcNow.AddHours(_appSettings.ExpirationHours),
-                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
-            });
+                AddError("User is temporarily blocked for too many wrong attempts to login.");
+                return CustomResponse();
+            }
 
-            var encodedToken = tokenHandler.WriteToken(token);
+            AddError("User or password is invalid.");
 
-            var response = new UserResponseLoginModel
-            {
-                AccessToken = encodedToken,
-                ExpiresIn = TimeSpan.FromHours(_appSettings.ExpirationHours).TotalSeconds,
-                UserToken = new UserTokenModel
-                {
-                    Id = user.Id,
-                    Email = user.Email,
-                    Claims = claims.Select(c => new UserClaimModel
-                    {
-                        Type = c.Type,
-                        Value = c.Value
-                    })
-                }
-            };
-
-            return response;
+            return CustomResponse();
         }
-
-        private static long ToUnixEpochDate(DateTime dateTime)
-            => (long) Math.Round((dateTime.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero))
-                .TotalSeconds);
     }
 }
--- src/services/Identity/NS.Identity.API/Startup.cs
+++ src/services/Identity/NS.Identity.API/Startup.cs
@@ -16,91 +16,47 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using NS.Identity.API.Configuration;
 using NS.Identity.API.Data;
 using NS.Identity.API.Extensions;
+using NS.Identity.API.Services;
 
 namespace NS.Identity.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
         public IConfiguration Configuration { get; }
         
+        public Startup(IHostEnvironment hostEnvironment)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(hostEnvironment.ContentRootPath)
+                .AddJsonFile("appsettings.json", true, true)
+                .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
+                .AddEnvironmentVariables();
+
+            if (hostEnvironment.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            Configuration = builder.Build();
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddIdentiyConfiguration(Configuration);
 
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddRoles<IdentityRole>()
-                .AddEntityFrameworkStores<ApplicationDbContext>()
-                .AddDefaultTokenProviders();
+            services.AddApiConfiguration();
 
-            //JWT
-            var appSettingsSection = Configuration.GetSection("AppSettings");
-            services.Configure<AppSettings>(appSettingsSection);
-
-            var appSettings = appSettingsSection.Get<AppSettings>();
-            var key = Encoding.ASCII.GetBytes(appSettings.Secret);
-
-            services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(bearerOptions =>
-            {
-                bearerOptions.RequireHttpsMetadata = true;
-                bearerOptions.SaveToken = true;
-                bearerOptions.TokenValidationParameters = new TokenValidationParameters()
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(key),
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = appSettings.ValidAt,
-                    ValidIssuer = appSettings.Issuer
-                };
-            });
-            
-            services.AddControllers();
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo
-                {
-                    Title = "NerdStore Identity API",
-                    Description = "Identity API",
-                    Contact = new OpenApiContact() {Name = "Alexandre Mutti", Email = "alexmutti@gmail.com"},
-                    License = new OpenApiLicense() {Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT")}
-                });
-            });
+            services.AddSwaggerConfiguration();
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-            });
+            app.UseSwaggerConfiguration();
 
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-            app.UseAuthentication();
-
-            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+            app.UseApiConfiguration(env);
         }
     }
 }
